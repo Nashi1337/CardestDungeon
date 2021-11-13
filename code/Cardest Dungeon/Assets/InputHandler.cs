f4e6f0ad0472d1444b41e52a6bbc323ac97c03d5
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2021-11-13 01:55:49 +0100

    Umsetzung der Kernmechanik angefangen

---
+++ code/Cardest Dungeon/Assets/InputHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public static DefaultInputActions Current
+    {
+        get
+        {
+            if(current == null)
+            {
+                current = new DefaultInputActions();
+                current.Enable();
+            }
+            return current;
+        }
+    }
+
+    private static DefaultInputActions current = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
