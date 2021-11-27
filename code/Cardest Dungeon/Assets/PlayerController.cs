0962ea57f6b8def3fa2807a81d0f200c7b83ad49
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2021-11-27 20:05:03 +0100

    InputManager vereinfacht

--- code/Cardest Dungeon/Assets/PlayerController.cs
+++ code/Cardest Dungeon/Assets/PlayerController.cs
@@ -8,40 +8,15 @@ public class PlayerController : MonoBehaviour
     private float speed;
     private Rigidbody2D rig = default;
 
-    public AudioClip test;
-
-    public GameObject canvas;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-
-        InputManager.Current.onMove += OnMove;
-        InputManager.Current.onAction1Down += OnAction1Down;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-
-    private void OnMove(Vector2 input)
-    {
-        rig.velocity = input * speed;
-    }
-
-    private void OnAction1Down()
-    {
-        Debug.Log("Action1 wurde gedrückt");
-
-        var audio = gameObject.GetComponent<AudioSource>();
-        if (audio == null)
-        {
-            audio = gameObject.AddComponent<AudioSource>();
-            audio.clip = test;
-        }
-        audio.Play();
-
+        rig.velocity = InputManager.CalculateMovement() * speed;
     }
 }
