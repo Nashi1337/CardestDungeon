452a773a663f2d6f7e528abc7a5977baee9bd597
Max Rogner <maxirogner@googlemail.com>
2022-01-23 22:06:55 +0100

    neue testszene hinzugefügt: tür+schlüssel funzt

---
+++ code/Cardest Dungeon/Assets/Scripts/KeyHolder.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHolder : MonoBehaviour
+{
+    private List<Key.KeyType> keyList;
+
+    private void Awake()
+    {
+        keyList = new List<Key.KeyType>();
+    }
+
+    public void AddKey(Key.KeyType keyType)
+    {
+        Debug.Log("Added key: " + keyType);
+        keyList.Add(keyType);
+    }
+
+    public void Removekey(Key.KeyType keyType)
+    {
+        keyList.Remove(keyType);
+    }
+
+    public bool ContainsKey(Key.KeyType keyType)
+    {
+        return keyList.Contains(keyType);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "key")
+        {
+            Key key = collider.GetComponent<Key>();
+            AddKey(key.GetKeyType());
+            Destroy(key.gameObject);
+        }
+        else
+        {
+            Debug.Log("Halts Maul");
+        }
+
+        KeyDoor keyDoor = collider.GetComponent<KeyDoor>();
+        if (keyDoor != null)
+        {
+            if (ContainsKey(keyDoor.GetKeyType()))
+            {
+                // Currently holding Key to open this door
+                Removekey(keyDoor.GetKeyType());
+                keyDoor.OpenDoor();
+            }
+        }
+    }
+
+}
