440a4884aee2ebd597f2e0eba443da4c636a84a9
Max Rogner <maxirogner@googlemail.com>
2022-05-09 21:29:33 +0200

    evil projectile angepasst. wird nun in richtung player gefeuert und kollidiert mit wänden

--- code/Cardest Dungeon/Assets/Scripts/TEST/EvilProjectile.cs
+++ code/Cardest Dungeon/Assets/Scripts/TEST/EvilProjectile.cs
@@ -8,45 +8,55 @@ public class EvilProjectile : MonoBehaviour
     private int damage = 3;
     //0 = SW, 1 = NW, 2 = NE, 3 = SE
     private float direction = 0;
-    //private int accelerationFactor = 1;
+
 
     PlayerController target;
     Vector3 targetpos;
+    Vector3 targetDir;
 
     private void Start()
     {
-        target = FindObjectOfType<PlayerController>();
-        targetpos = target.GetComponent<Transform>().localScale;
-        if (targetpos.x < transform.position.x && targetpos.y < transform.position.y)
-        {
-            direction = 0f;
-            //accelerationFactor = -1; 
-            Debug.Log(direction);
-        }
-        else if (targetpos.x > transform.position.x && targetpos.y < transform.position.y)
-        {
-            direction = 3f;
-            //accelerationFactor = 1;
-            Debug.Log(direction);
-        }
-        else if (targetpos.x > transform.position.x && targetpos.y > transform.position.y)
-        {
-            direction = 2f;
-            //accelerationFactor = 1;
-            Debug.Log(direction);
-        }
-        else if (targetpos.x < transform.position.x && targetpos.y > transform.position.y)
-        {
-            direction = 1f;
-            //accelerationFactor = 1;
-            Debug.Log(direction);
-        }
+        /*        target = FindObjectOfType<PlayerController>();
+                targetpos = target.GetComponent<Transform>().localScale;
+                if (targetpos.x < transform.position.x && targetpos.y < transform.position.y)
+                {
+                    direction = 0f;
+                    //accelerationFactor = -1; 
+                    Debug.Log(direction);
+                }
+                else if (targetpos.x > transform.position.x && targetpos.y < transform.position.y)
+                {
+                    direction = 3f;
+                    //accelerationFactor = 1;
+                    Debug.Log(direction);
+                }
+                else if (targetpos.x > transform.position.x && targetpos.y > transform.position.y)
+                {
+                    direction = 2f;
+                    //accelerationFactor = 1;
+                    Debug.Log(direction);
+                }
+                else if (targetpos.x < transform.position.x && targetpos.y > transform.position.y)
+                {
+                    direction = 1f;
+                    //accelerationFactor = 1;
+                    Debug.Log(direction);
+                }*/
         //damage = Inventory.instance.GetMagicModifier();
+
+        targetDir = (PlayerController.Current.transform.position - transform.position).normalized;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += targetDir * Time.fixedDeltaTime * moveSpeed;
     }
 
     private void Update()
     {
-        if (direction == 0)
+/*        if (direction == 0)
         {
             transform.position = new Vector3(
             transform.position.x - (moveSpeed * Time.deltaTime
@@ -81,7 +91,7 @@ public class EvilProjectile : MonoBehaviour
                 transform.position.y + (moveSpeed * Time.deltaTime),
                 transform.position.z
                 );
-        }
+        }*/
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
