9baad43bb8fa6b04fdd74feed1749d7a8a35f817
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2022-05-10 13:46:02 +0200

    Es ist fast wie wasser

---
+++ code/Cardest Dungeon/Assets/Water/DirectionChanger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionChanger : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector2 newDirection;
+}
