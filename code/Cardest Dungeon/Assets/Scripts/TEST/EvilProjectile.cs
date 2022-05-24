4b4cab84d865f6784164fdafb15489435ff7160b
Max <a@b.com>
2022-05-24 13:42:06 +0200

    kleinere balancing updates map angepasst

--- code/Cardest Dungeon/Assets/Scripts/TEST/EvilProjectile.cs
+++ code/Cardest Dungeon/Assets/Scripts/TEST/EvilProjectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EvilProjectile : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    public float moveSpeed = 10f;
     public int damage = 6;
 
     Vector3 targetDir;
--- code/Cardest Dungeon/Assets/Scripts/TEST/ProjectileTEST.cs
+++ code/Cardest Dungeon/Assets/Scripts/TEST/ProjectileTEST.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ProjectileTEST : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    public float moveSpeed = 15f;
     public int damage;
     private bool left = false;
 
