112e8ade38f111495c513a6f8078e25280f301f7
Max <a@b.com>
2022-05-30 13:48:46 +0200

    kleine Änderungen an der Playtesting Szene. nichts weltbewegendes

--- code/Cardest Dungeon/Assets/Scripts/Water/NotWaterProof.cs
+++ code/Cardest Dungeon/Assets/Scripts/Water/NotWaterProof.cs
@@ -18,6 +18,7 @@ public class NotWaterProof : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Ich bin " + this.name + " und bin mit " + collision.gameObject.name + " kollidiert");
         if(collision.gameObject.GetComponent<WaterScript>() != null)
         {
             Destroy(gameObject);
