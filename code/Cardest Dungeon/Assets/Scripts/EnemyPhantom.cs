507972a7a15348e6eaf621fe4d730590cda88c61
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2022-07-11 10:09:56 +0200

    Alle drei Verhalten des Phantoms implementiert (Feuer, Eis, Normal)

--- code/Cardest Dungeon/Assets/Scripts/EnemyPhantom.cs
+++ code/Cardest Dungeon/Assets/Scripts/EnemyPhantom.cs
@@ -2,45 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MethodStateMachine;
+using System;
 
 public class EnemyPhantom : MonoBehaviour
 {
-    //General
+    [Serializable]
+    struct Behaviour
+    {
+        public float moveTime;
+        public float idleTime;
+        public float acceleration;
+        public float sightRadius;
+        public float retreatTime;
+    }
+
     [SerializeField]
     private float moveRadius;
+    [SerializeField]
+    private Behaviour ice_Behaviour;
+    [SerializeField]
+    private Behaviour fire_Behaviour;
+    [SerializeField]
+    private Behaviour normal_Behaviour;
 
     private float stateTimer;
     private Element mainState;
+    private Behaviour active_Behaviour;
     private Vector3 origin;
     private Vector2 moveDirection;
     private Animator animator;
     private Rigidbody2D rb;
     private SpriteRenderer spriterenderer;
     private MapPiece homeArea;
-
-    //Icebehaviour
-    [SerializeField]
-    private float ice_moveTime;
-    [SerializeField]
-    private float ice_idleTime;
-    [SerializeField]
-    private float ice_acceleration;
-
-    private StateMachine ice_Behaviour;
-
-    //FireBehaviour
-    [SerializeField]
-    private float fire_moveTime;
-    [SerializeField]
-    private float fire_idleTime;
-    [SerializeField]
-    private float fire_acceleration;
-    [SerializeField]
-    private float fire_sightRadius;
-    [SerializeField]
-    private float fire_retreatTime;
-
-    private StateMachine fire_Behaviour;
+    private StateMachine stateMachine;
 
     void Start()
     {
@@ -51,27 +45,27 @@ public class EnemyPhantom : MonoBehaviour
         spriterenderer = GetComponent<SpriteRenderer>();
         homeArea = MapManager.Current.FindClosestMapPieceByPosition(transform.position);
 
-        ice_Behaviour = new StateMachine("Init", Ice_Init);
-        ice_Behaviour.AddState("Idle", Ice_Idling);
-        ice_Behaviour.AddState("Move", Ice_Moving);
-        ice_Behaviour.AddTransition("Idle", "Move", Ice_IdlingToMoving);
-        ice_Behaviour.AddTransition("Move", "Idle", Ice_MovingToIdle);
-        ice_Behaviour.AddTransition("Init", "Idle", Ice_MovingToIdle);
+        //ice_Behaviour = new StateMachine("Init", Ice_Init);
+        //ice_Behaviour.AddState("Idle", Ice_Idling);
+        //ice_Behaviour.AddState("Move", Ice_Moving);
+        //ice_Behaviour.AddTransition("Idle", "Move", Ice_IdlingToMoving);
+        //ice_Behaviour.AddTransition("Move", "Idle", Ice_MovingToIdle);
+        //ice_Behaviour.AddTransition("Init", "Idle", Ice_MovingToIdle);
 
-        fire_Behaviour = new StateMachine("Init", Fire_Init);
-        fire_Behaviour.AddState("Idle", Fire_Idling);
-        fire_Behaviour.AddState("Move", Fire_Moving);
-        fire_Behaviour.AddState("AttackPlayer", Fire_Attacking);
-        fire_Behaviour.AddState("RetreatFromPlayer", Fire_Retreating);
-        fire_Behaviour.AddTransition("Idle", "Move", Fire_IdlingToMoving);
-        fire_Behaviour.AddTransition("Move", "Idle", Fire_MovingToIdle);
-        fire_Behaviour.AddTransition("Init", "Idle", Fire_MovingToIdle);
-        fire_Behaviour.AddTransition("AttackPlayer", "RetreatFromPlayer", Fire_ToRetreat);
-        fire_Behaviour.AddTransition("RetreatFromPlayer", "Idle", Fire_ToIdle);
+        stateMachine = new StateMachine("Init", Fire_Init);
+        stateMachine.AddState("Idle", Fire_Idling);
+        stateMachine.AddState("Move", Fire_Moving);
+        stateMachine.AddState("AttackPlayer", Fire_Attacking);
+        stateMachine.AddState("RetreatFromPlayer", Fire_Retreating);
+        stateMachine.AddTransition("Idle", "Move", Fire_IdlingToMoving);
+        stateMachine.AddTransition("Move", "Idle", Fire_MovingToIdle);
+        stateMachine.AddTransition("Init", "Idle", Fire_MovingToIdle);
+        stateMachine.AddTransition("AttackPlayer", "RetreatFromPlayer", Fire_ToRetreat);
+        stateMachine.AddTransition("RetreatFromPlayer", "Idle", Fire_ToIdle);
 
         UpdateMainState();
 
-        Debug.LogWarning("Sprite flippen und moveDirection ändern in eine Methode packen");
+        Debug.LogWarning("Der Verhaltenswechsel ingame wurde noch nicht getestet.");
     }
 
     //This empty method is necessary in order to overwrite the methods from Enemy
@@ -82,35 +76,24 @@ public class EnemyPhantom : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(stateTimer >= 0)
+        if (stateTimer >= 0)
         {
             stateTimer -= Time.fixedDeltaTime;
         }
 
-        switch(mainState)
+        stateMachine.Run();
+
+        string activeState = stateMachine.GetActiveStateName();
+        if (!activeState.Equals("AttackPlayer")
+            && !activeState.Equals("RetreatFromPlayer")
+            && IsPlayerInSight()
+            )
         {
-            case Element.ICE:
-                ice_Behaviour.Run();
-                break;
-            case Element.FIRE:
-                Debug.Log(fire_Behaviour.GetActiveStateName());
-                fire_Behaviour.Run();
-
-                string activeState = fire_Behaviour.GetActiveStateName();
-                if (!activeState.Equals("AttackPlayer")
-                    && !activeState.Equals("RetreatFromPlayer")
-                    && IsPlayerInSight()
-                    )
-                {
-                    Debug.LogWarning("Attack");
-                    fire_Behaviour.TransitionToState("AttackPlayer");
-                }
-                break;
+            stateMachine.TransitionToState("AttackPlayer");
         }
-
     }
 
-    private void UpdateMainState()
+    public void UpdateMainState()
     {
         mainState = MapManager.Current.FindElementOfMapPiece(homeArea);
 
@@ -118,113 +101,66 @@ public class EnemyPhantom : MonoBehaviour
         {
             case Element.ICE:
                 animator.speed = 0.6f;
+                SwitchBehaviour(ice_Behaviour);
                 break;
             case Element.FIRE:
                 animator.speed = 1.3f;
+                SwitchBehaviour(fire_Behaviour);
                 break;
             case Element.NONE:
                 animator.speed = 1f;
+                SwitchBehaviour(normal_Behaviour);
                 break;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(mainState == Element.FIRE && collision.gameObject.tag.Equals("Player"))
+        if(mainState != Element.ICE && stateMachine.GetActiveStateName().Equals("AttackPlayer") && collision.gameObject.tag.Equals("Player"))
         {
             Debug.LogWarning("Retreat from Player should probaly happen in another place. Recheck this after adding enemy behaviour to Phantom.");
-            fire_Behaviour.TransitionToState("RetreatFromPlayer");
+            stateMachine.TransitionToState("RetreatFromPlayer");
         }
     }
 
-    #region IceBehaviour
-    private void Ice_Init()
-    {
-        ice_Behaviour.TransitionToState("Idle");
-    }
-
-    private void Ice_Idling()
-    {
-        if(stateTimer <= 0)
-        {
-            ice_Behaviour.TransitionToState("Move");
-        }
-    }
-
-    private void Ice_Moving()
-    {
-        rb.AddForce(moveDirection * ice_acceleration);
-
-        if (stateTimer <= 0)
-        {
-            ice_Behaviour.TransitionToState("Idle");
-        }
-    }
-
-    private void Ice_IdlingToMoving()
-    {
-        animator.SetBool("isMoving", true);
-        if ((origin - transform.position).magnitude < moveRadius)
-        {
-            moveDirection.x = Random.Range(-1f, 1f);
-            moveDirection.y = Random.Range(-1f, 1f);
-            moveDirection.Normalize();
-        }
-        else
-        {
-            moveDirection *= -1;
-        }
-        spriterenderer.flipX = moveDirection.x < 0;
-
-        stateTimer = Random.Range(ice_moveTime * 0.8f, ice_moveTime * 1.2f);
-    }
-
-    private void Ice_MovingToIdle()
-    {
-        animator.SetBool("isMoving", false);
-
-        stateTimer = Random.Range(ice_idleTime * 0.8f, ice_idleTime * 1.2f);
-    }
-    #endregion
-
-    #region FireBehaviour
+    #region StateMachine
     private void Fire_Init()
     {
-        fire_Behaviour.TransitionToState("Idle");
+        stateMachine.TransitionToState("Idle");
     }
 
     private void Fire_Idling()
     {
         if(stateTimer <= 0)
         {
-            fire_Behaviour.TransitionToState("Move");
+            stateMachine.TransitionToState("Move");
         }
     }
 
     private void Fire_Moving()
     {
-        rb.AddForce(moveDirection * fire_acceleration);
+        rb.AddForce(moveDirection * active_Behaviour.acceleration);
 
         if(stateTimer <= 0)
         {
-            fire_Behaviour.TransitionToState("Idle");
+            stateMachine.TransitionToState("Idle");
         }
     }
 
     private void Fire_Attacking()
     {
         moveDirection = (PlayerController.Current.transform.position - transform.position).normalized;
-        rb.AddForce(moveDirection * fire_acceleration);
+        rb.AddForce(moveDirection * active_Behaviour.acceleration);
         spriterenderer.flipX = moveDirection.x < 0;
     }
 
     private void Fire_Retreating()
     {
-        rb.AddForce(moveDirection * fire_acceleration * 0.5f);
+        rb.AddForce(moveDirection * active_Behaviour.acceleration * 0.5f);
 
         if(stateTimer <= 0)
         {
-            fire_Behaviour.TransitionToState("Idle");
+            stateMachine.TransitionToState("Idle");
         }
     }
 
@@ -233,8 +169,8 @@ public class EnemyPhantom : MonoBehaviour
         animator.SetBool("isMoving", true);
         if ((origin - transform.position).magnitude < moveRadius)
         {
-            moveDirection.x = Random.Range(-1f, 1f);
-            moveDirection.y = Random.Range(-1f, 1f);
+            moveDirection.x = UnityEngine.Random.Range(-1f, 1f);
+            moveDirection.y = UnityEngine.Random.Range(-1f, 1f);
             moveDirection.Normalize();
         }
         else
@@ -243,21 +179,25 @@ public class EnemyPhantom : MonoBehaviour
         }
         spriterenderer.flipX = moveDirection.x < 0;
 
-        stateTimer = Random.Range(fire_moveTime * 0.8f, fire_moveTime * 1.2f);
+        mainState = MapManager.Current.FindElementOfMapPiece(homeArea);
+
+        stateTimer = UnityEngine.Random.Range(active_Behaviour.moveTime * 0.8f, active_Behaviour.moveTime * 1.2f);
     }
 
     private void Fire_MovingToIdle()
     {
         animator.SetBool("isMoving", false);
 
+        mainState = MapManager.Current.FindElementOfMapPiece(homeArea);
+        
         Fire_ToIdle();
     }
 
     private void Fire_ToIdle()
     {
-        Debug.Log("Ich wurde gerufen");
+        mainState = MapManager.Current.FindElementOfMapPiece(homeArea);
 
-        stateTimer = Random.Range(fire_idleTime * 0.8f, fire_idleTime * 1.2f);
+        stateTimer = UnityEngine.Random.Range(active_Behaviour.idleTime * 0.8f, active_Behaviour.idleTime * 1.2f);
     }
 
     private void Fire_ToRetreat()
@@ -265,12 +205,17 @@ public class EnemyPhantom : MonoBehaviour
         moveDirection = -(PlayerController.Current.transform.position - transform.position).normalized;
         spriterenderer.flipX = moveDirection.x < 0;
 
-        stateTimer = fire_retreatTime;
+        stateTimer = active_Behaviour.retreatTime;
     }
     #endregion
 
     private bool IsPlayerInSight()
     {
-        return (PlayerController.Current.transform.position - transform.position).magnitude < fire_sightRadius;
+        return (PlayerController.Current.transform.position - transform.position).magnitude < active_Behaviour.sightRadius;
+    }
+
+    private void SwitchBehaviour(Behaviour newBehaviour)
+    {
+        active_Behaviour = newBehaviour;
     }
 }
